1938cd8f2b5d23b72acaa7f035547cf49f8b2f63
Ahsan Rasheed <ahsanrasheed135@gmail.com>
2023-07-15 19:59:59 +0500

    spinner

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -3,13 +3,18 @@ import { Form, Input, message } from "antd";
 import "../styles/RegisterStyles.css";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { showLoading, hideLoading } from "../redux/features/alertSlice";
 const Register = () => {
     //new comment
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     //form handler
     const onfinishHandler = async (values) => {
         try {
+            dispatch(showLoading());
             const res = await axios.post("/api/v1/user/register", values);
+            dispatch(hideLoading());
             if (res.data.success) {
                 message.success("User Registered Successfully");
                 navigate("/login");
@@ -17,6 +22,7 @@ const Register = () => {
                 message.error(res.data.message);
             }
         } catch (error) {
+            dispatch(hideLoading());
             console.log("Errpr");
             message.error("Something went wrong");
         }
