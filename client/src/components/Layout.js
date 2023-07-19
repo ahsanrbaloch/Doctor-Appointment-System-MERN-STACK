91695aa690aaff0fed1e5bbbc7f6cf76b8d4e043
Ahsan Rasheed <ahsanrasheed135@gmail.com>
2023-07-19 18:43:48 +0500

    roles

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "../styles/LayoutStyles.css";
-import { SidebarMenu } from "../Data/data";
-import { Link, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { UserMenu, adminMenu } from "../Data/data";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { message } from "antd";
+import { clearUser } from "../redux/features/userSlice";
 
 const Layout = ({ children }) => {
     const { user } = useSelector((state) => state.user);
     const location = useLocation();
+    const dispatch = useDispatch();
 
+    //logout function
+    const handleLogout = () => {
+        localStorage.clear();
+        dispatch(clearUser());
+        message.success("Logout Successfully");
+    };
+    //rendering menu list
+    const SidebarMenu = user?.isAdmin ? adminMenu : UserMenu;
     return (
         <>
             <div className="main">
@@ -33,6 +44,10 @@ const Layout = ({ children }) => {
                                     </div>
                                 );
                             })}
+                            <div className={"menu-item"} onClick={handleLogout}>
+                                <i className="fa-solid fa-right-from-bracket"></i>
+                                <Link to="/login">Logout</Link>
+                            </div>
                         </div>
                     </div>
                     <div className="content">
