bca70ce7ec5e0a6993883d230c1994e11e6bd1f9
Ahsan Rasheed <ahsanrasheed135@gmail.com>
2023-07-21 02:26:09 +0500

    notification

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,22 +1,44 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { showLoading } from "../redux/features/alertSlice";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { setUser } from "../redux/features/userSlice";
+import { useEffect } from "react";
 // import axios from "axios";
 
 export default function ProtectedRoute({ children }) {
-    // const dispatch = useDispatch();
-    // const { user } = useSelector((state) => state.user);
+    const { user } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
 
-    //get user
-    // const getUser = async () => {
-    //     try {
-    //         dispatch(showLoading);
-    //         const res = await axios.post("/api/v1/user/getUser");
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
+    useEffect(() => {
+        const getUserData = async () => {
+            try {
+                const res = await axios.post(
+                    "/api/v1/user/getUserData",
+                    {},
+                    {
+                        headers: {
+                            Authorization:
+                                "Bearer " + localStorage.getItem("token"),
+                        },
+                    }
+                );
+                if (res.data.success) {
+                    dispatch(setUser(res.data.data));
+                } else {
+                    <Navigate to="/login" />;
+                    localStorage.clear();
+                }
+            } catch (error) {
+                localStorage.clear();
+                console.log(error);
+            }
+        };
+
+        if (!user) {
+            getUserData();
+        }
+    }, [dispatch, user]);
     if (localStorage.getItem("token")) {
         return children;
     } else return <Navigate to="/login" />;
--- client/src/pages/NotificationPage.js
+++ client/src/pages/NotificationPage.js
@@ -1,12 +1,94 @@
 import React from "react";
 import Layout from "../components/Layout";
-
+import { Tabs, message } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { hideLoading, showLoading } from "../redux/features/alertSlice";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "../redux/features/userSlice";
+import axios from "axios";
 const NotificationPage = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const { user } = useSelector((state) => state.user);
+    //handle read notification
+    const handleMarkAllRead = async () => {
+        try {
+            dispatch(showLoading());
+            const res = await axios.post(
+                "/api/v1/user/get-all-notification",
+                {
+                    userId: user._id,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                            "token"
+                        )}`,
+                    },
+                }
+            );
+            dispatch(hideLoading());
+            if (res.data.success) {
+                message.success(res.data.message);
+                dispatch(setUser(res.data.data));
+            } else {
+                message.error(res.data.message);
+            }
+        } catch (error) {
+            dispatch(hideLoading());
+            console.log(error);
+            message.error("Something went wrong");
+        }
+    };
+    const handleDeleteAllRead = () => {};
     return (
         <Layout>
-            <div>
-                <h1>Notification Page</h1>
-            </div>
+            <h4 className="p-3 text-center">Notification Page</h4>
+            <Tabs>
+                <Tabs.TabPane tab="unRead" key={0}>
+                    <div className="d-flex justify-content-end">
+                        <h4
+                            className="p-2"
+                            onClick={handleMarkAllRead}
+                            style={{ cursor: "pointer" }}
+                        >
+                            Mark All Read
+                        </h4>
+                    </div>
+                    {user?.notification?.map((notification) => (
+                        <div className="card" style={{ cursor: "pointer" }}>
+                            <div
+                                className="card-text"
+                                onClick={() =>
+                                    navigate(notification.onClickPath)
+                                }
+                            >
+                                {notification.message}
+                            </div>
+                        </div>
+                    ))}
+                </Tabs.TabPane>
+
+                <Tabs.TabPane tab="Read" key={1}>
+                    <div className="d-flex justify-content-end">
+                        <h4 className="p-2" onClick={handleDeleteAllRead}>
+                            Delete All Read
+                        </h4>
+                    </div>
+                    {user?.seennotification.map((notification) => (
+                        <div className="card" style={{ cursor: "pointer" }}>
+                            <div
+                                className="card-text"
+                                onClick={() =>
+                                    navigate(notification.onClickPath)
+                                }
+                            >
+                                {notification.message}
+                            </div>
+                        </div>
+                    ))}
+                </Tabs.TabPane>
+            </Tabs>
         </Layout>
     );
 };
